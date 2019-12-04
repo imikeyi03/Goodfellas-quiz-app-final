cb02b3cebac9500196b2b1ba42ce353f08a9cd8f
Mikey Mann <mikey.mannx3@gmail.com>
2019-12-04 11:31:46 -0800

    created HTML, CSS, and JS file with STORE array of all questions and answers

---
+++ index.js
@@ -0,0 +1,71 @@
+const STORE = {
+    questions: [
+        
+        // Question 1
+        {
+            question: "Who kills loudmouth Morrie?",
+            choices: [
+                "Henry",
+                "The Joker",
+                "Tommy",
+                "Vinnie"
+            ],
+            answer: "Henry"
+        },
+       
+        // Question 2
+        {
+            question: "What African-American actor played Stack Edwards?",
+            choices: [
+                "Morgan Freeman",
+                "Samuel L. Jackson",
+                "Ice T",
+                "Michael Jordan"
+            ],
+            answer: "Samuel L. Jackson"
+        },
+    
+    
+         // Question 3
+         {
+            question: "What U.S city does Henry heavily distribute cocaine to?",
+            choices: [
+                "Pittsburgh",
+                "Los Angeles",
+                "Seattle",
+                "Dallas"
+            ],
+            answer: "Pittsburgh"
+        },
+    
+
+         // Question 4
+         {
+            question: "How many F-bombs are dropped during the course of the film?",
+            choices: [
+                "108",
+                "203",
+                "90",
+                "300"
+            ],
+            answer: "300"
+        },
+    
+    
+     // Question 5
+        {
+            question: "Who directed the movie Goodfellas?",
+            choices: [
+                "Hugh Jackman",
+                "Martin Scorsesse",
+                "Danny Devito",
+                "JK Rowling"
+            ],
+            answer: "Martin Scorsesse"
+        }
+    
+    ],
+    currentQuestion: 0,
+    score: 0
+};
+
